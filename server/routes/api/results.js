0e3f78e9c64bdb62044abe0f8b55d909a6f0a7de
Mitja Potocin <mitja.potocin@gmail.com>
2019-07-25 21:52:06 +0200

    Saved before changing sendToDB

--- server/routes/api/results.js
+++ server/routes/api/results.js
@@ -4,21 +4,23 @@ require("dotenv").config();
 
 const router = express.Router();
 //change name of posts to results, change add posts requirements
+
 // GET Results
 router.get("/", async (req, res) => {
   const results = await loadResultsCollection();
   res.send(await results.find({}).toArray());
 });
 
-// GET Results search my name
-router.get("/:Name", async (req, res) => {
+
+// GET Results search my course
+router.get("/:course", async (req, res) => {
   const results = await loadResultsCollection();
   res.send(await results.find({
-    name: req.params.name
+    Course: req.params.course
   }).toArray());
 });
 
-// Add Reults
+// Add Results
 router.post("/", async (req, res) => {
   const results = await loadResultsCollection();
   await results.insertOne(req.body, (err, result) => {
@@ -28,56 +30,15 @@ router.post("/", async (req, res) => {
     res.status(201).send()
 
   })
-
-
-  // await results.insertOne({
-  //   Name: req.body.Name,
-  //   Team: req.body.Team,
-  //   Name: req.body.Name,
-  //   Time: req.body.Time,
-
-  //   PlacePoints: req.body.PlacePoints,
-
-  // "Team": "GKD BOROVCI",
-  // "Club": "",
-  // "Nation": "SLO",
-  // "Birth": "",
-  // "Course": "3 Stages BBF",
-  // "Category": "Moški 25-29",
-  // "Time": "00:10:50,000",
-  // "Behind": "00:00:47,960",
-  // "Penalty": "",
-  // "1.Control(Name)": "Stage 1",
-  // "1.Splittime": "00:04:09,100",
-  // "1.Split-Pos.": 8,
-  // "1.Split-Behind": "00:00:19,710",
-  // "1.Total Time": "00:04:09,100",
-  // "1.Total Pos.": 8,
-  // "1.Total-Behind": "00:00:19,710",
-  // "2.Control(Name)": "Stage 2",
-  // "2.Splittime": "00:04:38,360",
-  // "2.Split-Pos.": 11,
-  // "2.Split-Behind": "00:00:24,020",
-  // "2.Total Time": "00:08:47,460",
-  // "2.Total Pos.": 11,
-  // "2.Total-Behind": "00:00:43,730",
-  // "3.Control(Name)": "Stage 3",
-  // "3.Splittime": "00:02:02,540",
-  // "3.Split-Pos.": 8,
-  // "3.Split-Behind": "00:00:04,580",
-  // "3.Total Time": "00:10:50,000",
-  // "3.Total Pos.": 10,
-  // "3.Total-Behind": "00:00:47,960",
-  // "": ""
-
 });
 
-async function loadResultsCollection() {
+//Conect to DB /w default collection  "Results"
+async function loadResultsCollection(collection = "Results") {
   const client = await mongodb.MongoClient.connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true
   });
 
-  return client.db("sloenduro-stats").collection("Results");
+  return client.db("sloenduro-stats").collection(collection);
 }
 
 module.exports = router;
