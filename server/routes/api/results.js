beb3247d1ac6fd431c6e2f786504b215aad35dc9
Mitja Potocin <mitja.potocin@gmail.com>
2019-08-09 20:47:56 +0200

    added racers route

--- server/routes/api/results.js
+++ server/routes/api/results.js
@@ -10,25 +10,40 @@ router.get("/", async (req, res) => {
   res.send(await results.find({}).toArray());
 });
 
-
 // GET Results search by event
 router.get("/:event", async (req, res) => {
   const results = await loadResultsCollection();
-  res.send(await results.find({
-    event: req.params.event
-  }).toArray());
+  res.send(
+    await results
+      .find({
+        event: req.params.event
+      })
+      .toArray()
+  );
+});
+
+// GET Results search by name
+router.get("/riders/:name", async (req, res) => {
+  const results = await loadResultsCollection();
+  let name = new RegExp(req.params.name, "i");
+  res.send(
+    await results
+      .find({
+        Name: name
+      })
+      .toArray()
+  );
 });
 
 // Add Results
 router.post("/", async (req, res) => {
   const results = await loadResultsCollection();
   await results.insertOne(req.body, (err, result) => {
-    if (err) return console.log(err)
+    if (err) return console.log(err);
 
-    console.log('saved to database')
-    res.status(201).send()
-
-  })
+    console.log("saved to database");
+    res.status(201).send();
+  });
 });
 
 //Connect to DB /w default collection  "Results"
