4fa585693d89e95ad3d879dc5e30432f169fb027
mitjapotocin <mitja.potocin@gmail.com>
2020-10-06 05:04:55 +0200

    Added snake api

--- server/index.js
+++ server/index.js
@@ -1,37 +1,39 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const history = require("connect-history-api-fallback");
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const history = require('connect-history-api-fallback')
+const app = express()
 
 // Middleware
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.json())
+app.use(cors())
 
-const results = require("./routes/api/results");
-const events = require("./routes/api/events");
+const results = require('./routes/api/results')
+const events = require('./routes/api/events')
+const snake = require('./routes/api/snake-results')
 
-app.use(express.static(__dirname + "/upload"));
+app.use(express.static(__dirname + '/upload'))
 
-app.use("/api/results", results);
-app.use("/api/events", events);
+app.use('/api/results', results)
+app.use('/api/events', events)
+app.use('/api/snake-results', snake)
 
 // Handle production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname + "/public/"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/public/'))
 
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 //For history mode in Vue-Router
-const staticFileMiddleware = express.static(__dirname);
-app.use(staticFileMiddleware);
+const staticFileMiddleware = express.static(__dirname)
+app.use(staticFileMiddleware)
 app.use(
   history({
     disableDotRule: true,
-    verbose: true
+    verbose: true,
   })
-);
-app.use(staticFileMiddleware);
+)
+app.use(staticFileMiddleware)
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server started on port ${port}`))
---
+++ server/routes/api/snake-results.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongodb = require('mongodb')
+require('dotenv').config()
+
+const router = express.Router()
+
+// GET Results
+router.get('/', async (req, res) => {
+  const results = await loadResultsCollection()
+  res.send(await results.find({}).toArray())
+})
+
+// Add Results
+router.post('/', async (req, res) => {
+  const results = await loadResultsCollection()
+  await results.insertOne(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.status(201).send()
+  })
+})
+
+//Conect to DB /w default collection  "Results"
+async function loadResultsCollection() {
+  const client = await mongodb.MongoClient.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+  })
+  return client.db('sloenduro-stats').collection('snake-results')
+}
+
+module.exports = router
